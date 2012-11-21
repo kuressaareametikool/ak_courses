85236fe8e8b447b897187e75e513aef99fef3613
Kristjan Jansen <kristjan.jansen@gmail.com>
2012-11-21 17:15:26 +0200

    modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+var ds = new Miso.Dataset({
+  importer : Miso.Dataset.Importers.GoogleSpreadsheet,
+  parser : Miso.Dataset.Parsers.GoogleSpreadsheet,
+  key : "0AjuiYlqIySuFdDdUYlY1YkNzOWtJSWxVOWwyTS0zWUE",
+  worksheet : "1",
+  fast : true,
+  sync: true
+});
+
+
+$(".thumbnails").on("click", "li", function(event) {
+  var id = $(this).data('id');
+    
+  var data = ds.fetch({ 
+    success : function() {
+      var data = this.rows(function(row) {
+        return row.id === id;
+      }).rowByPosition(0);
+      
+      $.Mustache.load('templates/modal.mustache')
+        .done(function () {
+          $('#modal').empty().mustache('modal', data).modal('show');        
+        })    
+      
+      }
+  });
+    
+
+    
+});
+
 showResults();
 
 $("#types a").toggle(function () {
@@ -12,13 +43,7 @@ $("#types a").toggle(function () {
 
 function showResults(type) {
 
-  var ds = new Miso.Dataset({
-    importer : Miso.Dataset.Importers.GoogleSpreadsheet,
-    parser : Miso.Dataset.Parsers.GoogleSpreadsheet,
-    key : "0AjuiYlqIySuFdDdUYlY1YkNzOWtJSWxVOWwyTS0zWUE",
-    worksheet : "1",
-    fast : true
-  });
+
 
   var rows = []
 
